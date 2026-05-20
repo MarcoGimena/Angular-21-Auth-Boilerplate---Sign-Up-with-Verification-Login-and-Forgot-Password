fb871aa92ea46c0c073428e9e063bd0ccf89444e
Marco Gimena <marcogimena401@gmail.com>
2026-05-20 11:37:21 +0800

    feat: direct root level angular deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://loyola-api.onrender.com'
+  production: true,
+  apiUrl: 'https://node-js-typescript-mysql-boilerplate-api-uu57.onrender.com'
 };
